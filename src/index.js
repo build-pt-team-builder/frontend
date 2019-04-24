40a17f1c9c30cee961ea599b0d79a6ff69ee876a
Dustin Snoap <afrobacon@gmail.com>
2019-04-24 00:57:58 -0400

    Installed Yarn Added to yarn:  redux, styled-components, react-router-dom, axios Added to local: reset.css Removed: App.test.js, logo.svg, App.js, index.css, App.css

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+//REACT
+import React from 'react'
+import ReactDOM from 'react-dom'
+//REDUX
+//LOCAL
+import './reset.css'
+//DELETE?
+import * as serviceWorker from './serviceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const root = document.getElementById('root')
+ReactDOM.render(<h1>Nope</h1>, root)
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+//DELETE?
 serviceWorker.unregister();
