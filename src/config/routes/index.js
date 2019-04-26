bd482af0ac945459255021cfde30f310c9501e60
Wilfred Morgan <wil.me.morgan@gmail.com>
2019-04-26 21:51:35 +0800

    Add routes for project list and project details

--- src/config/routes/index.js
+++ src/config/routes/index.js
@@ -2,26 +2,44 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { Route, withRouter } from 'react-router-dom'
 // import PrivateRoute from './PrivateRoute'
+
+import { fetchProjectData } from '../../actions/projects'
+import ProjectList from '../../views/Projects/ProjectList'
+import ProjectDetails from '../../views/Projects/ProjectDetails'
+
+
 class Routes extends Component {
-//   componentDidMount() {
-//     this.props.getData()
-//   }
+  // componentDidMount() {
+  //   this.props.fetchData()
+  // }
 
   render() {
     console.log(`Routes render this.props is: `)
     return (
       <div>
         {/* Assign routes */}
-        <Route />
+        <Route path="/" exact component={ProjectList} />
+        {this.props.projects.map(project => (
+          <Route
+            key={project.id}
+            path={`/projects/${project.id}`}
+            render={props => (
+              <ProjectDetails {...props} project={project} />
+            )}
+          />
+        ))}
       </div>
     )
   }
 }
 
-// const mapStateToProps = ({  }) => ({
- 
-// })
+const mapStateToProps = state => {
+  console.log(state.projects)
+  return {
+    projects: state.projects.projects
+  }
+}
 
 export default withRouter(
-  connect(null, {})(Routes)
+  connect(mapStateToProps, { fetchData: fetchProjectData })(Routes)
 )
