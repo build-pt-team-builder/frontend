5a5404bb3f96c12f1606cea6692c088a7ae5fd08
Wilfred Morgan <wil.me.morgan@gmail.com>
2019-05-01 18:02:26 +0800

    Add styling for Role component
    Update font sizes

--- src/components/RoleComponents/RoleStyleComponents.js
+++ src/components/RoleComponents/RoleStyleComponents.js
@@ -1,12 +1,11 @@
 import styled from 'styled-components'
-import { flex } from '../DesignComponents/theme'
+import { colorScheme, fontSizing, flex } from '../DesignComponents/theme'
 
 
 export const RoleFormContainer = styled.div`
-width: 100%;
-
+  width: 100%;
   ${flex('row', 'center', 'space-between')}
-
+  
   form {
     width: 80%;
     input {
@@ -19,6 +18,7 @@ width: 100%;
 export const FormContainer = styled.form`
   width: 90%;
   max-width: 600px;
+  font-size: ${fontSizing.s};
 `
 
 export const FormGroup = styled.div`
@@ -27,10 +27,60 @@ export const FormGroup = styled.div`
   
   input {
     width: 42%;
+    padding: 5px;
+    font-size: ${fontSizing.s};
   }
 
   button {
     width: 10%;
   }
+`
 
+export const RoleListContainer = styled.div`
+    width: 90%;
+    max-width: 600px;
+    ${flex('column','flex-start')};
+
+    ul {
+      width: 100%;
+      padding: 0;
+      list-style-type: none;
+    }
+`
+
+export const RoleContainer = styled.div`
+    width: 100%;
+    ${flex('row', 'center', 'flex-start')}
+    margin: 5px 0;
+    padding-left: 10px;
+    border: 1px solid ${colorScheme.defaultBorderColor};
+    border-radius: 5px;
+
+    div {
+      flex: 0 0 43%;
+      padding: 5px 0;
+      font-size: ${fontSizing.s};
+    }
+
+    div:last-child {
+      justify-self: 'flex-end';
+      flex: 0 0 14%;
+    }
+
+    i {
+      justify-self: 'flex-end';
+      font-size: ${fontSizing.s};
+    }
+`
+
+export const DeleteContainer = styled.div`
+  ${flex('column','flex-end')};
+
+  .fa-ellipsis-v, .fa-edit {
+    margin: 5px;
+  }
+
+  .fa-ellipsis-v, .fa-trash, .fa-edit {
+    cursor: pointer;
+  }  
 `
