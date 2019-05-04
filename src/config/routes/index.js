ec5c88750ca9094d9bb11cf2698982eb539062d2
Dustin Snoap <afrobacon@gmail.com>
2019-05-04 11:45:30 -0400

    Proceeding work on alternative project list

--- src/config/routes/index.js
+++ src/config/routes/index.js
@@ -6,9 +6,7 @@ import {connect} from 'react-redux'
 // Action creators
 import { fetchProjectData } from '../../actions/projects'
 // Components
-import ProjectList from '../../views/Projects/ProjectList'
-import ProjectDetails from '../../views/Projects/ProjectDetails'
-import ProjectForm from '../../views/Projects/ProjectForm'
+import Projects from '../../views/Projects/'
 import LandingPage from '../../components/LandingPage'
 import Login from '../../views/Login'
 
@@ -32,27 +30,7 @@ class Routes extends Component {
         <Route path='/users' component={UserList} />
         <Route path='/announcements' component={Announcements} />
         
-        <Route path="/projects" exact component={ProjectList} />
-        <Route 
-          path="/projects/add" exact 
-          render={props => <ProjectForm {...props} add/>}
-        />
-        {this.props.projects.map((project) => (
-          <Route
-            key={project.id}
-            path={`/projects/${project.id}`}
-            render={props => (
-              <ProjectDetails {...props} project={project} />
-            )}
-          />
-        ))}
-        {this.props.projects.map((project) => (
-          <Route
-            key={project.id}
-            path={`/projects/update/${project.id}`}
-            render={props => <ProjectForm {...props} project={project} update />}
-          />
-        ))}
+        <Route path="/projects" exact component={Projects} />
       </div>
     )
   }
