2109bcbfe9176c44a942d9b82bcad7e6a68d7dea
Wilfred Morgan <wil.me.morgan@gmail.com>
2019-05-04 09:27:39 +0800

    Add sort and filter functionality

--- src/views/Projects/ProjectList.js
+++ src/views/Projects/ProjectList.js
@@ -3,24 +3,31 @@ import { connect } from 'react-redux'
 import ReactTable from 'react-table'
 import 'react-table/react-table.css'
 import { TableContainer } from './ProjectStyleComponents'
+import { color } from '../../components/DesignComponents/theme'
+import { categories } from '../../dummyData'
 
 class ProjectTable extends Component {
   render() {
     const { projects } = this.props
     const columns = [
-      {
-        Header: "Project Name",
+    { 
+      Header: 'Build Week Projects',
+      columns: [{   
+        Header: 'Name',
         accessor: "name",
         width: 150
       },
       {
-        Header: "Pitch",
+        Header: "Description",
         accessor: "pitch",
+        filterable: false,
         width: 350
+        
       },
       {
         Header: "Roles",
         id: "roleAssignments",
+        filterable: false,
         width: 300,
         accessor: d => {
           return (
@@ -44,13 +51,94 @@ class ProjectTable extends Component {
             />
           )
         }
+      },
+      {
+        Header: "Status",
+        id: "projectComplete",
+        width: 105,
+        accessor: d => d.projectComplete ? 'Complete' : 'Open',
+        Cell: row => (
+          <div
+            style={{
+              textAlign: 'center',
+              color: row.value === 'Complete' ? `${color.danger}` : `dodgerblue`,
+              fontWeight: 'bold'
+            }}
+
+          >
+          {row.value}
+          </div>
+        ),
+        filterMethod: (filter, row) => {
+          if (filter.value === 'all') {
+            return true
+          }
+          if (filter.value === 'Open') {
+            return row[filter.id] === 'Open'
+          } else {
+            return row[filter.id] === 'Complete'
+          }
+        },
+        Filter: ({ filter, onChange }) =>
+          <select
+            onChange={e => onChange(e.target.value)}
+            value={filter ? filter.value : 'all'}
+          >
+            <option value='all'>All</option>
+            <option value='Open'>Open</option>
+            <option value='Complete'>Complete</option>
+          </select>
+      },
+      {
+        Header: "Category",
+        id: 'category',
+        accessor: d => d.category,
+        Cell: row => (
+          <div
+            style={{
+              textAlign: 'center',
+              width: '100%'
+            }}
+
+          >
+            {row.value}
+          </div>
+        ),
+        filterMethod: (filter, row) => {
+          if (filter.value === 'all') {
+            return true
+          }
+          if (filter.value === row.category) {
+            return row[filter.id] === filter.value
+          }
+        },
+        Filter: ({ filter, onChange }) =>
+          <select
+            onChange={e => onChange(e.target.value)}
+            value={filter ? filter.value : 'all'}
+          >
+            <option value='all'>All</option>
+            {
+              categories.map(category => (
+                <>
+                  <option key={category.id} value={category.name}>{category.name}</option>
+                </>
+              ))
+            }
+            <option value='end'>End</option>
+          </select>
       }
+    ]
+    },
+
     ]
     return (
       <TableContainer>
-        <ReactTable 
+        <ReactTable
+          getTheadThProps={this.hideHeader} 
           columns={columns}
           data={projects}
+          filterable
           defaultPageSize={5}
           className="-striped -highlight"
           getTrProps={(state, rowInfo, column, instance) => {
